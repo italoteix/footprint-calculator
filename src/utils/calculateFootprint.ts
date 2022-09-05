18d1854740aac6560d5784ed8b7845909cddba93
Ítalo Teixeira <italo.brtx@gmail.com>
2022-09-06 01:14:04 +0200

    feat(utils): move calculate footprint to utils

--- src/utils/calculateFootprint.ts
+++ src/utils/calculateFootprint.ts
@@ -1,13 +1,14 @@
 import { TravelType } from '../model';
 
 interface CalculateFootprintArgs {
-  distance: number;
+  distance: string;
   numberOfTravelers: number;
   type: TravelType;
 }
 
 const C02_G_PER_PASSENGER_PER_KM = 285;
 const GRAMS_TO_KG = 1000;
+const DISTANCE_FLOAT_PARSER = 100;
 
 const travelTypeRatio: Record<TravelType, number> = {
   ONE_WAY: 1,
@@ -19,8 +20,10 @@ export const calculateFootprint = ({
   numberOfTravelers,
   type
 }: CalculateFootprintArgs) => {
+  const parsedDistance = Number(distance) * DISTANCE_FLOAT_PARSER;
+
   return (
-    (distance * numberOfTravelers * travelTypeRatio[type] * C02_G_PER_PASSENGER_PER_KM) /
-    GRAMS_TO_KG
+    (parsedDistance * numberOfTravelers * travelTypeRatio[type] * C02_G_PER_PASSENGER_PER_KM) /
+    (GRAMS_TO_KG * DISTANCE_FLOAT_PARSER)
   ).toFixed(2);
 };
